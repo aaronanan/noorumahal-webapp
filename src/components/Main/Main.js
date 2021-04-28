68cc0349f7df727de6651f1cf04dcbadb07ac9f7
harishanan <harish7673@hotmail.com>
2021-04-27 19:39:48 -0700

    main layout complete

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -27,18 +27,20 @@ var items = [
 ];
 function Item(props) {
   return (
-    <Paper class="text-center">
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
-
-      {/* <Button className="CheckButton">Check it out!</Button> */}
-    </Paper>
+    <div class="row justify-content-center align-items-center reviews-box">
+      <div class="col-12 col-lg-6">
+        <Paper class="text-center">
+          <h2>{props.item.name}</h2>
+          <p>{props.item.description}</p>
+        </Paper>
+      </div>
+    </div>
   );
 }
 const Main = () => {
   return (
     <>
-      <div class="container-fluid main-top" style={{ paddingBottom: "200px" }}>
+      <div class="container-fluid main-top">
         <MediaQuery minWidth={599}>
           <div class="row">
             <div class="container-image">
@@ -103,11 +105,21 @@ const Main = () => {
         </div>
 
         <div class="row">
+          <div class="col-12">
+            <Carousel animation="slide">
+              {items.map((item, i) => (
+                <Item key={i} item={item} />
+              ))}
+            </Carousel>
+          </div>
+        </div>
+
+        <div class="row">
           <div
             class="col-lg-6 p-3 text-center bg-dark"
             style={{ height: "350px", color: "white" }}
           >
-            <div style={{ marginTop: "20px" }}>
+            <div class="top-space">
               <div class="h3">Hours of Operation</div>
               <div class="h6">Monday: 5pm - 10pm</div>
               <div class="h6">Tuesday: 5pm - 10pm</div>
@@ -119,57 +131,53 @@ const Main = () => {
             </div>
           </div>
           <div
-            class="col-lg-6 p-3 text-center bg-light border"
-            style={{ height: "350px", color: "black" }}
+            class="col-lg-6 p-3 text-center bg-light border hours-contact"
+            style={{ color: "black" }}
           >
-            <div class="row">
-              <div class="col-12">
-                <p class="h3">Want to contact us? Send us a message</p>
+            <div class="top-space">
+              <div class="row">
+                <div class="col-12">
+                  <p class="h3">Want to contact us? Send us a message</p>
+                </div>
               </div>
+              <div class="row justify-content-center align-items-center top-space">
+                <div class="col-sm-5">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Name"
+                    // style={{ width: "300px" }}
+                  />
+                </div>
+                <div class="col-sm-5">
+                  <input
+                    type="email"
+                    class="form-control input-space"
+                    id="exampleFormControlInput1"
+                    placeholder="Email"
+                    // style={{ width: "300px" }}
+                  />
+                </div>
+              </div>
+              <div
+                class="row justify-content-center align-items-center"
+                style={{ marginTop: "15px" }}
+              >
+                <div class="col-sm-10">
+                  <textarea
+                    class="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    placeholder="Your Message"
+                    style={{ resize: "none" }}
+                  ></textarea>
+                </div>
+              </div>
+              <button type="button" class="btn btn-secondary top-space">
+                Order Now
+              </button>
             </div>
-            <div class="row justify-content-center align-items-center">
-              <div class="col-5">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Name"
-                  // style={{ width: "300px" }}
-                />
-              </div>
-              <div class="col-5">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Email"
-                  // style={{ width: "300px" }}
-                />
-              </div>
-            </div>
-            <div
-              class="row justify-content-center align-items-center"
-              style={{ marginTop: "15px" }}
-            >
-              <div class="col-10">
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                  placeholder="Your Message"
-                  style={{ resize: "none" }}
-                ></textarea>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <Carousel animation="slide">
-              {items.map((item, i) => (
-                <Item key={i} item={item} />
-              ))}
-            </Carousel>
           </div>
         </div>
       </div>
